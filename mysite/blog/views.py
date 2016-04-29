36e4f5f979525fe148ec0dba8b94fe1f93f8a00b
gusy39 <gusy39@163.com>
2016-04-29 23:11:54 +0800

    display models

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Person
 # Create your views here.
 def hello(request):
     return HttpResponse('<html>hello world!</html>')
 def list(request):
     l=["css","jquery","javascript","python","django","html"]
-    return render(request,'hello.html',{'list':l})
+    pl=Person.objects.all()
+    return render(request,'hello.html',{'list':pl})
