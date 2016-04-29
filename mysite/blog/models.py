794ee5b24130d796184f875ec826969320e10f94
gusy39 <gusy39@163.com>
2016-04-29 22:21:20 +0800

    add models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+       cname=models.CharField(max_length=30)
+class Person(models.Model):
+       Car=models.ForeignKey(Car)
+       pname=models.CharField(max_length=30)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,12 +75,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST':'192.168.2.199',
+        'PORT':'3306',
     }
-}
+} 
 
 
 # Password validation
